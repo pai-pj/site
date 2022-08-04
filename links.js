4ee2f53a05e9f26587520c6eaf77627624c2477b
Liliane Camargos <lilianecamargos@gmail.com>
2022-08-04 10:59:21 -0300

    menu de formação - ajustes

--- links.js
+++ links.js
@@ -2,49 +2,49 @@ var LINKS = [
   {
     "titulo": "Núcleos Regionais",
     "subtitulo": "Planilhas e Formulários",
-    "itens":[
-     
+    "itens": [
+
       {
-        "titulo":"Equipe PAI-PJ",
-        "url":"https://docs.google.com/spreadsheets/d/1f0G8IDzmoLyAgFhoqli9qa8QsBSMJjWsAeVslaAOhBQ/edit#gid=0"
+        "titulo": "Equipe PAI-PJ",
+        "url": "https://docs.google.com/spreadsheets/d/1f0G8IDzmoLyAgFhoqli9qa8QsBSMJjWsAeVslaAOhBQ/edit#gid=0"
       },
       {
-        "titulo":"Pacientes",
-        "itens":[
+        "titulo": "Pacientes",
+        "itens": [
           {
-            "titulo":"Dados Psicossociais",
-            "url":"https://docs.google.com/spreadsheets/d/1IY8d-9iTsIqBBsjWNHyuZ_S8rJ7lAG-V5zl2d1lxetw/edit#gid=47917158"
-           
+            "titulo": "Dados Psicossociais",
+            "url": "https://docs.google.com/spreadsheets/d/1IY8d-9iTsIqBBsjWNHyuZ_S8rJ7lAG-V5zl2d1lxetw/edit#gid=47917158"
+
           },
           {
-            "titulo":"Dados Processuais",
-            "url":"https://docs.google.com/spreadsheets/d/1UpTNPsWWvhGdP56wRdhmzgSrqdj9-bbHKpOdCtU0i1A/edit#gid=855660271"
-            
+            "titulo": "Dados Processuais",
+            "url": "https://docs.google.com/spreadsheets/d/1UpTNPsWWvhGdP56wRdhmzgSrqdj9-bbHKpOdCtU0i1A/edit#gid=855660271"
+
           }
         ]
-        
+
       },
       {
-        "titulo":"Registro de Ações",
-        "itens":[
+        "titulo": "Registro de Ações",
+        "itens": [
           {
-            "titulo":"Formulário",
-            "url":"https://docs.google.com/forms/d/e/1FAIpQLSdTJMtKWW9IkOChlK8Tg1lHYnTwQqcqfeNpKbw1UZX0NwbHIQ/viewform?usp=sf_link"
+            "titulo": "Formulário",
+            "url": "https://docs.google.com/forms/d/e/1FAIpQLSdTJMtKWW9IkOChlK8Tg1lHYnTwQqcqfeNpKbw1UZX0NwbHIQ/viewform?usp=sf_link"
           },
           {
-            "titulo":"Planilha",
-            "url":"https://docs.google.com/spreadsheets/d/1ruqv3khE5tZjPZPa90XsRe7dGNDtNE7-xhIcgR-PXFA/edit#gid=0"
+            "titulo": "Planilha",
+            "url": "https://docs.google.com/spreadsheets/d/1ruqv3khE5tZjPZPa90XsRe7dGNDtNE7-xhIcgR-PXFA/edit#gid=0"
           }
         ]
-        
+
       },
-      
+
       {
         "titulo": "Acolhimento",
         "itens": [
           {
             "titulo": "Formulário",
-            "url":"https://docs.google.com/forms/d/e/1FAIpQLSdE3VXPcSVACC0Qg2ABxAVghc-Y9zCzIB6AsO1ayPsQO1B4BA/viewform?usp=sf_link"
+            "url": "https://docs.google.com/forms/d/e/1FAIpQLSdE3VXPcSVACC0Qg2ABxAVghc-Y9zCzIB6AsO1ayPsQO1B4BA/viewform?usp=sf_link"
           },
           {
             "titulo": "Planilha",
@@ -54,18 +54,18 @@ var LINKS = [
       },
 
       {
-        "titulo":"Entrada",
-        "itens":[
+        "titulo": "Entrada",
+        "itens": [
           {
-            "titulo":"Formulário",
-            "url":"https://docs.google.com/forms/d/e/1FAIpQLSek3bwuAAFCv7_aBTcDOPDKwlcJIDnneuwPGwl5y-szIwn9dw/viewform?usp=sf_link"
+            "titulo": "Formulário",
+            "url": "https://docs.google.com/forms/d/e/1FAIpQLSek3bwuAAFCv7_aBTcDOPDKwlcJIDnneuwPGwl5y-szIwn9dw/viewform?usp=sf_link"
           },
           {
-            "titulo":"Planilha",
-            "url":"https://docs.google.com/spreadsheets/d/13rDDwDjlTq3SFWuOX3JiaINy7dsapxlVTcRD6rJdAPg/edit#gid=1496529896"
+            "titulo": "Planilha",
+            "url": "https://docs.google.com/spreadsheets/d/13rDDwDjlTq3SFWuOX3JiaINy7dsapxlVTcRD6rJdAPg/edit#gid=1496529896"
           }
         ]
-        
+
       },
 
       {
@@ -73,7 +73,7 @@ var LINKS = [
         "itens": [
           {
             "titulo": "Formulário",
-            "url":"https://docs.google.com/forms/d/e/1FAIpQLSey9HplzgVYg5QC5Fa02fwhWIY9MkWKdr9sHKVXkRuewVseuQ/viewform?usp=sf_link"
+            "url": "https://docs.google.com/forms/d/e/1FAIpQLSey9HplzgVYg5QC5Fa02fwhWIY9MkWKdr9sHKVXkRuewVseuQ/viewform?usp=sf_link"
           },
           {
             "titulo": "Planilha",
@@ -99,140 +99,168 @@ var LINKS = [
         ]
       }
       */
-      
+
     ]
   },
   {
     "titulo": "Núcleo Supervisor",
     "subtitulo": "Planilhas acessadas pelo Núcleo Supervisor",
-    "itens":[
+    "itens": [
       {
-        "titulo":"Supervisões",
-        "url":"https://docs.google.com/spreadsheets/d/1-0GLHgwBF7u3do6mvPCFNHRJ1842guvFtJyvW4LHJCk/edit#gid=1755192778"
+        "titulo": "Supervisões",
+        "url": "https://docs.google.com/spreadsheets/d/1-0GLHgwBF7u3do6mvPCFNHRJ1842guvFtJyvW4LHJCk/edit#gid=1755192778"
       },
       {
-        "titulo":"Registro de Ações - NS",
-        "url":"https://docs.google.com/spreadsheets/d/1R47qI1CmEaUK2hp-SABQ5K_65VhcUQIkO9ifejUyvCs/edit#gid=1686231361"
+        "titulo": "Registro de Ações - NS",
+        "url": "https://docs.google.com/spreadsheets/d/1R47qI1CmEaUK2hp-SABQ5K_65VhcUQIkO9ifejUyvCs/edit#gid=1686231361"
       },
       {
-        "titulo":"Registro de Ações - Supervisão Clínica",
-        "url":"https://docs.google.com/spreadsheets/d/1rjTVFjoVv2ZTcMo-xZXCjWPkcXy9U29A_zkuKERTjSM/edit?usp=sharing"
-      },         
+        "titulo": "Registro de Ações - Supervisão Clínica",
+        "url": "https://docs.google.com/spreadsheets/d/1rjTVFjoVv2ZTcMo-xZXCjWPkcXy9U29A_zkuKERTjSM/edit?usp=sharing"
+      },
     ]
   },
   {
     "titulo": "Formação",
     "subtitulo": "Links e sites com conteúdo relevante",
-    "itens":[
+    "itens": [
       {
-        "titulo":"Sessões Clínicas e Reuniões",
-        "url":" https://docs.google.com/spreadsheets/d/1jtBnisgFAiF0_2ZqEjSQJxh-AgrZ4KVtMwdtqRz5PSM/edit"
-      },
-           {
-        "titulo":"Curso de Formação",
-        "url":"https://docs.google.com/spreadsheets/d/1rkPTzHODBrhNGwCN7QfYxj9ZhVHmAxms7FYj_OLIyL8/edit#gid=0"
+        "titulo": "Sessões Clínicas e Reuniões",
+        "url": " https://docs.google.com/spreadsheets/d/1jtBnisgFAiF0_2ZqEjSQJxh-AgrZ4KVtMwdtqRz5PSM/edit"
       },
       {
-        "titulo":"Revista Responsabilidades",
-        "url":"https://bd.tjmg.jus.br/jspui/handle/tjmg/577/browse?type=dateissued&submit_browse=Exibir+todos+os+itens"
+        "titulo": "Curso de Formação",
+        "url": "https://docs.google.com/spreadsheets/d/1rkPTzHODBrhNGwCN7QfYxj9ZhVHmAxms7FYj_OLIyL8/edit#gid=0"
       },
       {
-        "titulo":"Canal do Youtube",
-        "url":"https://www.youtube.com/channel/UC-MeM32WFAkA02hHhGfdBrQ/playlists",
+        "titulo": "Revista Responsabilidades",
+        "url": "https://bd.tjmg.jus.br/jspui/handle/tjmg/577/browse?type=dateissued&submit_browse=Exibir+todos+os+itens"
+      },
+      {
+        "titulo": "Canal do Youtube",
+        "url": "https://www.youtube.com/channel/UC-MeM32WFAkA02hHhGfdBrQ/playlists",
         "invisivel": true
       },
       {
-        "titulo":"Documentários",
-        "itens":[
+        "titulo": "Vídeos no Youtube",
+        "itens": [
           {
-            "titulo":"PAI-PJ na Audiência de Custódia 2020",
-            "url":"https://youtu.be/pNsZAToICAU"
-            
+            "titulo":"Em Nome da Razão - 1979",
+            "url":"https://youtu.be/cvjyjwI4G9c"
           },
           {
-            "titulo":"Justiça em Questão - 2018",
-            "url":"https://youtu.be/aAFgMQrfrp8"
+            "titulo":"A Casa dos Mortos",
+            "url":"https://youtu.be/noZXWFxdtNI"
           },
           {
-            "titulo":"Programa Inclusão - Crime e Castigo - Louco Infrator",
-            "url":"https://www.youtube.com/watch?v=TARtii1-H4c&feature=em-share_video_user"
-            
+            "titulo":"Holocausto Brasileiro",
+            "url":"https://youtu.be/139n1x24ILU"
           },
           {
-            "titulo":"A cidadania do louco infrator - 2009",
-            "url":"https://youtu.be/WNbUTHr0kHQ"
-            
+            "titulo": "PAI-PJ na Audiência de Custódia 2020",
+            "url": "https://youtu.be/pNsZAToICAU"
+          },
+          {
+            "titulo": "Justiça em Questão - 2018",
+            "url": "https://youtu.be/aAFgMQrfrp8"
+          },
+          {
+            "titulo": "Programa Inclusão - Crime e Castigo - Louco Infrator",
+            "url": "https://www.youtube.com/watch?v=TARtii1-H4c&feature=em-share_video_user"
+          },
+          {
+            "titulo": "A cidadania do louco infrator - 2009",
+            "url": "https://youtu.be/WNbUTHr0kHQ"
+          },
+          {
+            "titulo": "Seminário Formativo - Saúde Mental na Interface com Justiça Criminal - 2022",
+            "url": "https://youtu.be/VI8FeT88tOo"
           }
         ]
       },
       {
-        "titulo":"Primeira Jornada Clinica PAI-PJ",
+        "titulo": "Textos",
+        "itens": [
+          {
+            "titulo": "Textos de referência",
+            "url": "https://drive.google.com/drive/folders/0B3bKwT4pTk35V2lqNE10QS0zb00?resourcekey=0-zIe8MDAe-wLWR4RuRFkFAA&usp=sharing"
+          },
+          {
+            "titulo": "Leis, Resoluções e Portarias",
+            "url": "https://drive.google.com/drive/folders/1fCNjAJ1TLmyemu8JuhGfFlbLHIGwB2E_?usp=sharing"
+          }
+        ]
+      },
+
+
+      {
+        "titulo": "Primeira Jornada Clinica PAI-PJ",
         "invisivel": true,
-        "itens":[
+        "itens": [
           {
-            "titulo":"Parte 1",
-            "url":"https://www.youtube.com/watch?v=Q2fgS-S2sH8&feature=youtu.be"
+            "titulo": "Parte 1",
+            "url": "https://www.youtube.com/watch?v=Q2fgS-S2sH8&feature=youtu.be"
           },
           {
-            "titulo":"Parte 2",
-            "url":"https://www.youtube.com/watch?v=Qe0uAMwyWq4&feature=youtu.be"
+            "titulo": "Parte 2",
+            "url": "https://www.youtube.com/watch?v=Qe0uAMwyWq4&feature=youtu.be"
           }
         ]
       },
       {
-        "titulo":"Seminário de Ensino - Transferência na neurose e na psicose - 01/08/2017",
+        "titulo": "Seminário de Ensino - Transferência na neurose e na psicose - 01/08/2017",
         "invisivel": true,
         "url": "https://youtu.be/3NrUZePDHX0"
       },
       {
-        "titulo":"Seminário de Ensino - Valois - 13/12/2018",
+        "titulo": "Seminário de Ensino - Valois - 13/12/2018",
         "invisivel": true,
         "itens": [
           {
-            "titulo":"Parte 1",
-            "url":"https://youtu.be/_QzpSmbSe_E"
+            "titulo": "Parte 1",
+            "url": "https://youtu.be/_QzpSmbSe_E"
           },
           {
-            "titulo":"Parte 2",
-            "url":"https://youtu.be/g6r8lTXV71Y"
+            "titulo": "Parte 2",
+            "url": "https://youtu.be/g6r8lTXV71Y"
           }
         ]
       },
       {
-        "titulo":"Seminário de Ensino - 14/12/2018",
+        "titulo": "Seminário de Ensino - 14/12/2018",
         "invisivel": true,
         "itens": [
           {
-            "titulo":"Marcus André - manhã - parte I",
-            "url":"https://youtu.be/AtSXgSDVkMY"
+            "titulo": "Marcus André - manhã - parte I",
+            "url": "https://youtu.be/AtSXgSDVkMY"
           },
           {
-            "titulo":"Marcus André e Ana Lúcia - manhã - parte II",
-            "url":"https://youtu.be/8mCvrTn_B2A"
+            "titulo": "Marcus André e Ana Lúcia - manhã - parte II",
+            "url": "https://youtu.be/8mCvrTn_B2A"
           },
           {
-            "titulo":"Marcus André e Ana Lúcia - manhã - parte II",
-            "url":"https://youtu.be/8mCvrTn_B2A"
+            "titulo": "Marcus André e Ana Lúcia - manhã - parte II",
+            "url": "https://youtu.be/8mCvrTn_B2A"
           },
           {
-            "titulo":"Marcus André e Ana Lúcia - manhã - parte III",
-            "url":"https://youtu.be/af8l9vD73xo"
+            "titulo": "Marcus André e Ana Lúcia - manhã - parte III",
+            "url": "https://youtu.be/af8l9vD73xo"
           },
           {
-            "titulo":"Marcus André e Ana Lúcia - manhã - parte IV",
-            "url":"https://youtu.be/egeKd9DNZoQ"
+            "titulo": "Marcus André e Ana Lúcia - manhã - parte IV",
+            "url": "https://youtu.be/egeKd9DNZoQ"
           },
           {
-            "titulo":"Marcus André - tarde - parte I",
-            "url":"https://youtu.be/OOyLJMTJeKI"
+            "titulo": "Marcus André - tarde - parte I",
+            "url": "https://youtu.be/OOyLJMTJeKI"
           },
           {
-           "titulo":"Marcus André - tarde - parte II",
-            "url":"https://youtu.be/bV1BUAyfNII"
+            "titulo": "Marcus André - tarde - parte II",
+            "url": "https://youtu.be/bV1BUAyfNII"
           },
           {
-            "titulo":"Marcus André - tarde - parte III",
-            "url":"https://youtu.be/lxeSF_gWies"
+            "titulo": "Marcus André - tarde - parte III",
+            "url": "https://youtu.be/lxeSF_gWies"
           }
         ]
       }
@@ -244,59 +272,59 @@ var LINKS = [
   {
     "titulo": "Coordenação",
     "subtitulo": "Lista de Planilhas",
-    "itens": 
-    [ 
-      {
-        "titulo": "Análise dos dados",
-        "url":"https://docs.google.com/spreadsheets/d/1FdLNhJZv3nvH649yzEv5xaj2XSzJz6Uc4Z7EnaNx6F4/edit#gid=27897097"
-      },
-      
-      {
-        "titulo": "Revista Responsabilidades - Dados",
-        "url":"https://docs.google.com/spreadsheets/d/1zKqG1djEGikFp5_Lzo2aPEys-LCLDIrBO2lWWAUuzdY/edit#gid=0"
-      },
-      {
-        "titulo": "Revista Responsabilidades - Arquivos",
-        "url": "https://drive.google.com/drive/folders/16OlKtoGTuWoBb4wV-tG_CKzrlc5saob3?usp=sharing"
-      },
-      {
-        "titulo": "Relatórios",
-        "url": "https://drive.google.com/drive/folders/1-UGO2xkI-Zb06imWECus1c-aNquZ8riN?usp=sharing"
-      },
-     
-      {
-        "titulo": "Mutirões Barbacena",
-        "url":"https://docs.google.com/spreadsheets/d/159OKdzMIbvhl5ep-g00YRjQ9QLN10INYPv3DOu-Y2cY/edit#gid=0"
-      }         
-    ]
+    "itens":
+      [
+        {
+          "titulo": "Análise dos dados",
+          "url": "https://docs.google.com/spreadsheets/d/1FdLNhJZv3nvH649yzEv5xaj2XSzJz6Uc4Z7EnaNx6F4/edit#gid=27897097"
+        },
+
+        {
+          "titulo": "Revista Responsabilidades - Dados",
+          "url": "https://docs.google.com/spreadsheets/d/1zKqG1djEGikFp5_Lzo2aPEys-LCLDIrBO2lWWAUuzdY/edit#gid=0"
+        },
+        {
+          "titulo": "Revista Responsabilidades - Arquivos",
+          "url": "https://drive.google.com/drive/folders/16OlKtoGTuWoBb4wV-tG_CKzrlc5saob3?usp=sharing"
+        },
+        {
+          "titulo": "Relatórios",
+          "url": "https://drive.google.com/drive/folders/1-UGO2xkI-Zb06imWECus1c-aNquZ8riN?usp=sharing"
+        },
+
+        {
+          "titulo": "Mutirões Barbacena",
+          "url": "https://docs.google.com/spreadsheets/d/159OKdzMIbvhl5ep-g00YRjQ9QLN10INYPv3DOu-Y2cY/edit#gid=0"
+        }
+      ]
   },
 
   {
     "titulo": "Secretaria de Acolhimento",
     "subtitulo": "Lista de Planilhas",
-    "itens": 
-    [           
-      {
-        "titulo": "Entrada",
-        "url": "https://docs.google.com/spreadsheets/d/17s54iCrxGuUQMclkhik9595siG52RqKlXS2jIV_osbE/edit#gid=1279199042"        
-      },
-      {
-        "titulo": "Distribuição",
-        "url": "https://docs.google.com/spreadsheets/d/19IvDqWIbWzGhp4yITAq0SjxRnzMQ-bWQQt2XSQkCB7w/edit#gid=687444760"        
-      },
-      {
-        "titulo": "Consulta",
-        "url": "https://docs.google.com/spreadsheets/d/12rqh3ucErLydtY2fBgK502ZaqEF50EdZqUXSxY0g5pg/edit#gid=781383210"
-      },
-      {
-        "titulo": "Quantidade de Casos",
-        "url": "https://docs.google.com/spreadsheets/d/1TLrkIIkYZOOhynGaA4U0qcn6KVaBDxstdv_IcVI2sRo/edit#gid=0"        
-      },
-      {
-        "titulo": "Não listados",
-        "url": "https://docs.google.com/spreadsheets/d/1uMiz5gA3TCBkKsQILV1k6XUdgvWrLeQJTlVYPCayp4U/edit#gid=0"        
-      } 
-    ] 
+    "itens":
+      [
+        {
+          "titulo": "Entrada",
+          "url": "https://docs.google.com/spreadsheets/d/17s54iCrxGuUQMclkhik9595siG52RqKlXS2jIV_osbE/edit#gid=1279199042"
+        },
+        {
+          "titulo": "Distribuição",
+          "url": "https://docs.google.com/spreadsheets/d/19IvDqWIbWzGhp4yITAq0SjxRnzMQ-bWQQt2XSQkCB7w/edit#gid=687444760"
+        },
+        {
+          "titulo": "Consulta",
+          "url": "https://docs.google.com/spreadsheets/d/12rqh3ucErLydtY2fBgK502ZaqEF50EdZqUXSxY0g5pg/edit#gid=781383210"
+        },
+        {
+          "titulo": "Quantidade de Casos",
+          "url": "https://docs.google.com/spreadsheets/d/1TLrkIIkYZOOhynGaA4U0qcn6KVaBDxstdv_IcVI2sRo/edit#gid=0"
+        },
+        {
+          "titulo": "Não listados",
+          "url": "https://docs.google.com/spreadsheets/d/1uMiz5gA3TCBkKsQILV1k6XUdgvWrLeQJTlVYPCayp4U/edit#gid=0"
+        }
+      ]
   },
 
 
@@ -305,39 +333,39 @@ var LINKS = [
   {
     "titulo": "Administrativo",
     "subtitulo": "Lista de Planilhas",
-    "itens": 
-    [
-      {
-      "titulo": "Escala de trabalho - NRBH",
-      "url": "https://docs.google.com/spreadsheets/d/1OUGpQHP6MvbWSaLnAxuImEmY2OWSbS81eKHBNuB4OgA/edit#gid=1123041746"
-      },      
-      {
-        "titulo":"Solicitação de Transporte",
-        "itens":[
-          {
-            "titulo":"Formulário",
-            "url":"https://docs.google.com/forms/d/e/1FAIpQLScy-j6nUr4hv_JXcMDbbyEdurdhziDiv_ymagQ_LKyaWuCMkw/viewform?usp=sf_link"
-          },
-          {
-            "titulo":"Planilha",
-            "url":"https://docs.google.com/spreadsheets/d/1lBFW7hRrypSbQ8yGrp656kK_cawJQNo6v4jOlmDk95Q/edit#gid=1853555051"
-          }
-        ]
-        
-      },
-      {
-        "titulo": "Dados Funcionais",
-        "url":"https://docs.google.com/spreadsheets/d/1XirUfP0PL3VoXr221zAolv-hwxhPfseVzcMrQ4o9Ulc/edit#gid=2128006005"
-      },
-      {
-        "titulo": "Recebimento de Documentos/Ações em Teletrabalho",
-        "url":"https://docs.google.com/spreadsheets/d/19Oi5ntg5OSchBSayHiaqnrsX0HckG9YDQWHqw9Elpxc/edit#gid=487006680"
-      }
-    ]
+    "itens":
+      [
+        {
+          "titulo": "Escala de trabalho - NRBH",
+          "url": "https://docs.google.com/spreadsheets/d/1OUGpQHP6MvbWSaLnAxuImEmY2OWSbS81eKHBNuB4OgA/edit#gid=1123041746"
+        },
+        {
+          "titulo": "Solicitação de Transporte",
+          "itens": [
+            {
+              "titulo": "Formulário",
+              "url": "https://docs.google.com/forms/d/e/1FAIpQLScy-j6nUr4hv_JXcMDbbyEdurdhziDiv_ymagQ_LKyaWuCMkw/viewform?usp=sf_link"
+            },
+            {
+              "titulo": "Planilha",
+              "url": "https://docs.google.com/spreadsheets/d/1lBFW7hRrypSbQ8yGrp656kK_cawJQNo6v4jOlmDk95Q/edit#gid=1853555051"
+            }
+          ]
+
+        },
+        {
+          "titulo": "Dados Funcionais",
+          "url": "https://docs.google.com/spreadsheets/d/1XirUfP0PL3VoXr221zAolv-hwxhPfseVzcMrQ4o9Ulc/edit#gid=2128006005"
+        },
+        {
+          "titulo": "Recebimento de Documentos/Ações em Teletrabalho",
+          "url": "https://docs.google.com/spreadsheets/d/19Oi5ntg5OSchBSayHiaqnrsX0HckG9YDQWHqw9Elpxc/edit#gid=487006680"
+        }
+      ]
   }
 ];
-              
+
 var ESTILOS_NIVEIS = [
-["list-group-item-dark","list-group-item-secondary"],
-["list-group-item-primary","list-group-item-info"]
+  ["list-group-item-dark", "list-group-item-secondary"],
+  ["list-group-item-primary", "list-group-item-info"]
 ];  
